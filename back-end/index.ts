92a16c77c3da4cbbcbfd009c78b8d949422b92ef
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2023-07-31 13:08:16 +0700

    autoreload front-end

--- back-end/index.ts
+++ back-end/index.ts
@@ -1,1 +1,13 @@
-console.log("running")
+import livereload from "livereload"
+import connectLivereload from "connect-livereload"
+import path from "path";
+import express from "express";
+
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, './static'));
+
+const app = express();
+app.use(connectLivereload());
+app.use(express.static("./dist/static"))
+
+app.listen(8080)
--- dist/static/index.js
+++
@@ -1,1 +0,0 @@
-console.log("fklddngak sih")
