e24abb6efb31d35f3c8aa581c249e54a685e5a54
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2023-08-01 19:04:39 +0700

    Autoreload after restart server

--- back-end/index.ts
+++ back-end/index.ts
@@ -2,31 +2,49 @@ import livereload from "livereload"
 import connectLivereload from "connect-livereload"
 import path from "path";
 import express from "express";
-import { WebSocketServer } from "ws";
+import { WebSocket, WebSocketServer } from "ws";
 import { Server } from "http"
+import { Room } from "./room"
 
 const PORT = 8080
-const liveReloadServer = livereload.createServer();
+const app = express();
+const server = new Server(app);
+
+const liveReloadServer = livereload.createServer({}, () => {
+    const fnOut = (socket: WebSocket) => {
+        const fnIn = ({ data }: any) => {
+            if(JSON.parse(data).command === "hello"){
+                liveReloadServer.refresh(".")
+                socket.removeEventListener("message", fnIn)
+            }
+        }
+       socket.addEventListener("message", fnIn)
+       liveReloadServer.server.removeListener("connection", fnOut)
+    }
+    liveReloadServer.server.addListener("connection", fnOut)
+});
 liveReloadServer.watch(path.join(__dirname, './static'));
 
-const app = express();
-const server = new Server(app)
-
 app.use(connectLivereload());
-app.use(express.static("./dist/static"))
+app.use(express.static(path.join(__dirname, './static')))
 
 const wss = new WebSocketServer({ server })
 
-wss.on('connection', (ws) => {
+wss.on('connection', (ws, req) => {
     console.log("Connected")
 
     ws.on("message", (msg) => {
-        console.log(msg.toString())
+        try {
+            console.log(JSON.parse(msg.toString()))
+        } catch (error) {
+            console.log("-")
+        }
     })
 
     ws.on("close", ()=>{
         console.log("Disconnected")
     })
 })
+
 
 server.listen(PORT)
