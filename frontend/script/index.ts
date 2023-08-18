b1536aa5e6abd4c7eb8a56795ea63b6acb20118f
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2023-08-18 09:09:54 +0700

    Component typesystem

--- frontend/script/index.ts
+++ frontend/script/index.ts
@@ -1,27 +1,47 @@
 const app = document.getElementById("app") as HTMLDivElement
 
-type Tag = HTMLElementTagNameMap
-type ComponentOption<K extends keyof Tag> = {
-    [T in keyof Tag[K] as (Tag[K][T] extends string | boolean | number ? T : never)]?: Tag[K][T]
+type Beautify<T> = {
+    [K in keyof T]: T[K]
+} & {}
+
+type Assign<A extends object, B extends object> = {
+    [K in keyof (A & B)]: K extends keyof B ? B[K] : A[K & keyof A]
 }
 
-class Component<K extends keyof Tag>{
-    base: Tag[K]
-    el: HTMLElement
-
-    constructor(tagName: K, option?: ComponentOption<K>, childs?: string | HTMLElement[]) {
-        this.base = document.createElement(tagName)
-        Object.assign(this.base, option)
-        if (Array.isArray(childs)) childs.forEach(c => this.base.appendChild(c))
-        if (typeof childs === "string") this.base.innerHTML = childs
-
-        this.el = this.base
-    }
-
-    wrapWith(wrapper: HTMLElement){
-        wrapper.style.color = "red"
-        if (this.el.isConnected) this.el.parentNode?.insertBefore(wrapper, this.el)
-        wrapper.append(this.base)
-        this.el = wrapper
-    }
+type El = HTMLElementTagNameMap & { "element": HTMLElement }
+type ElOption<K extends keyof El> = {
+    [T in keyof El[K] as (El[K][T] extends string | boolean | number ? T : never)]?: El[K][T]
 }
+
+type Component<K extends keyof El, P extends object> = HTMLElement & ComponentCustomProps<K, P>
+
+type ComponentCustomProps<K extends keyof El, P extends object> = {
+    core: El[K],
+    ext: P
+}
+
+type Extension<K extends keyof El, P extends object> = (args: { core: El[K], el: HTMLElement }) => {
+    newEl?: HTMLElement
+    props: P
+}
+
+type ExtensionFactory<C extends object, K extends keyof El, P extends object> = (config: C) => Extension<K, P>
+
+type ExtensionInput = Extension<any, any> | { [key: string]: Extension<any, any> }
+
+type ExtractExtensionReturnAll<E extends ExtensionInput[]> = 
+    E extends [infer A extends ExtensionInput, ...infer B extends ExtensionInput[]] ? Beautify<Assign<ExtractExtensionReturn<A>, ExtractExtensionReturnAll<B>>> : {}
+
+type ExtractExtensionReturn<E extends ExtensionInput> = 
+    E extends Extension<infer _, infer P> ? P :
+    E extends object ? { [K in keyof E]: ExtractExtensionReturn<E[K]> } :
+    never
+
+function test<K extends keyof El, E extends ExtensionInput[]>(tagName: K, ...extensions: E): Component<K, ExtractExtensionReturnAll<E>> {
+    return undefined as any
+}
+
+const a = "" as any as Extension<"element", { a: string }>
+const b = "" as any as Extension<"element", { b: string }>
+
+const p = test("div", { a, b }, a)
