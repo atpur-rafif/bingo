923c117773dc36d1a405f86bb8d2d09337fd6d1b
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2023-08-19 01:39:26 +0700

    Waiting room visual

--- frontend/script/Menu.ts
+++ frontend/script/Menu.ts
@@ -1,5 +1,5 @@
 import { borderBelowAnimation } from "./Styling";
-import { child, createComponent, hideable } from "./Component";
+import { child, createComponent, hideable, styling } from "./Component";
 
 
 const Join = function (this: Menu) {
@@ -23,6 +23,7 @@ const Join = function (this: Menu) {
 
 const Create = function (this: Menu) {
     const createRoomButton = createComponent("button", { innerText: "Create" }, borderBelowAnimation);
+    createRoomButton.addEventListener("click", () => this.setState("waiting"))
     const createRoomInput = createComponent("input", { type: "text", placeholder: "Room Id" });
     const createBackButton = createComponent("button", { innerText: "Back" }, borderBelowAnimation);
     createBackButton.addEventListener("click", () => this.setState("option"))
@@ -53,18 +54,59 @@ const Option = function(this: Menu){
             shown: false
         }),
         child([
-            join, 
-            create
+            create, 
+            join
         ])
     );
 }
 
+const Waiting = function(this: Menu){
+    const user = ["A", "B", "C"]
 
+    const list = createComponent("div", {},
+        styling({
+            display: "flex",
+            flexDirection: "column"
+        })
+    )
+    user.forEach(v => {
+        list.append(createComponent("div", {innerText: v}))
+    })
+
+    const startButton = createComponent("button", { innerText: "Start Game" }, borderBelowAnimation) 
+    const cancelButton = createComponent("button", { innerText: "Cancel Game"}, borderBelowAnimation)
+    const buttonContainer = createComponent("div", {}, 
+        styling({
+            display: "flex",
+            flexDirection: "row",
+            gap: "1rem"
+        }),
+        child([startButton, cancelButton])
+    )
+
+    return createComponent("div", {},
+        styling({
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "1rem"
+        }),
+        hideable({
+            type: "height",
+            shown: false
+        }),
+        child([
+            list,
+            buttonContainer
+        ])
+    )
+}
 
 export class Menu {
     create = Create.apply(this)
     join = Join.apply(this)
     option = Option.apply(this)
+    waiting = Waiting.apply(this)
 
     el = createComponent("div", { className: "menu" },
         hideable({
@@ -72,15 +114,19 @@ export class Menu {
             shown: true
         }),
         child([
-            this.create, this.option, this.join
+            this.option, this.create, this.join, this.waiting
         ])
     );
 
     constructor() {
         this.setState("option");
+
+        setTimeout(() => {
+            this.setState("waiting")
+        }, 100);
     }
 
-    state: "option" | "create" | "join" = "option";
+    state: "option" | "create" | "join" | "waiting" = "option";
     setState(newState: typeof this.state) {
         [
             this.create,
